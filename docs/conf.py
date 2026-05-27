31be0b3355afb866eec295b35c011230ea30f0b6
hskobe <hskobe@uchicago.edu>
2026-05-27 14:55:51 -0400

    Fixed logos

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,7 @@ exclude_patterns = [
 # a list of builtin themes.
 #
 html_theme = 'sphinx_immaterial'
-html_logo = '_static/tina_backpop.png'
+html_logo = '_static/tina_circle.png'
 
 html_theme_options = {
 
@@ -153,7 +153,7 @@ html_theme_options = {
 
 html_last_updated_fmt = "%Y %b %d at %H:%M:%S UTC"
 html_show_sourcelink = False
-html_favicon = "_static/backpop-square.ico"
+html_favicon = "_static/tina_circle.png"
 
 # html_sidebars = {
 #     "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"],
